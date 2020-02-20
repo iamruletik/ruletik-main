e162fdf9412dc5100a5aefa3f1588f41d0c36cb4
Sasha Ruletik <iam@ruletik.com>
2020-02-20 18:36:38 +0300

    First Build

---
+++ build/precache-manifest.e003497b91ec5cdf96090cb5591bc7ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67cae289bbb8ffc53b35521f78138f2b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a0219a0fba4989bc688",
+    "url": "/static/css/main.500e6c9b.chunk.css"
+  },
+  {
+    "revision": "5bdf7356697e4bcc66d9",
+    "url": "/static/js/2.8ccfc826.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.8ccfc826.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a0219a0fba4989bc688",
+    "url": "/static/js/main.216d64ec.chunk.js"
+  },
+  {
+    "revision": "75355108fe426e098a48",
+    "url": "/static/js/runtime-main.a740767f.js"
+  }
+]);
