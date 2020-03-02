6d5754aa06e0e587cece089e92480e89f005949c
Sasha Ruletik <iam@ruletik.com>
2020-03-02 21:31:42 +0300

    Adding animation loop for scene & other changes

--- src/index.js
+++ src/index.js
@@ -3,61 +3,78 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import * as THREE from 'three'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 class App extends Component {
-  componentDidMount() {
-     // === THREE.JS CODE START ===
+	componentDidMount() {
+		// === THREE.JS CODE START ===
 
-    const width = window.innerWidth;
-    const height = window.innerHeight;
+		var mixer;
 
-    var gltfLoader = new GLTFLoader();
-    var scene = new THREE.Scene();
-    var camera = new THREE.OrthographicCamera( width / - 2, width / 2, height / 2, height / - 2, 1, 1000 );
-	var renderer = new THREE.WebGLRenderer();
+		var gltfLoader = new GLTFLoader();
+		var scene = new THREE.Scene();
+		scene.background = new THREE.Color( 0x808080 );
 
-	const ambientLight = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
+		var camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 1000 );
+		camera.position.set( 50, 50, 50 );
 
-	//var controls = new THREE.OrbitControls(camera, renderer.domElement);
-    
-    
-    
-	gltfLoader.load( './isometric-ruletik-test.glb', gltf => {
-		console.log(gltf);
- 		scene.add( gltf.scene );
- 		camera = gltf.cameras[ 0 ];
- 		scene.add( ambientLight );
-	} );
+		var renderer = new THREE.WebGLRenderer();
+		renderer.setPixelRatio( window.devicePixelRatio );
+		renderer.setSize( window.innerWidth, window.innerHeight );
+		this.mount.appendChild( renderer.domElement );
+
+		var hemiLight = new THREE.HemisphereLight( 0xffffff, 0x444444 );
+		hemiLight.position.set( 0, 20, 0 );
+		scene.add( hemiLight );
+
+		var dirLight = new THREE.DirectionalLight( 0xffffff );
+		dirLight.position.set( - 3, 10, - 10 );
+		scene.add( dirLight );
 
 
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    this.mount.appendChild( renderer.domElement );
-  
-    
-    var helper = new THREE.CameraHelper( camera );
-	scene.add( helper );
+		gltfLoader.load( './isometric-ruletik-test.glb', gltf => {
+			console.log(gltf);
+			scene.add( gltf.scene );
 
-    renderer.setClearColor(0x808080);
-    renderer.render(scene,camera);
-    
-    // === THREE.JS EXAMPLE CODE END ===
+			mixer = new THREE.AnimationMixer( gltf.scene );
+			mixer.clipAction( gltf.animations[ 0 ] ).play();
 
-  }
+		} );
 
- 
 
-  render() {
-    return (
-      <div ref={ref => (this.mount = ref)} />
-    )
-  }
+		const helper = new THREE.CameraHelper( camera );
+		scene.add( helper );
+
+
+		const controls = new OrbitControls( camera, renderer.domElement );
+
+		const clock = new THREE.Clock();
+
+		function animate() {
+
+			requestAnimationFrame( animate );
+
+			var delta = clock.getDelta();
+
+			if ( mixer ) mixer.update( delta );
+
+			renderer.render(scene,camera);
+
+		}
+
+		animate();
+
+		// === THREE.JS EXAMPLE CODE END ===
+
+	}
+
+	render() {
+		return (
+			<div ref={ref => (this.mount = ref)} />
+		)
+	}
 }
 
 export default App
 
-ReactDOM.render(<App />, document.getElementById('root')) 
-
-
-
-
-
+ReactDOM.render(<App />, document.getElementById('root'))
